f67c5fa40fabe2d1369ee6aa88a0330e87241809
Christina McMahon <christina.mcmahon1@gmail.com>
2019-12-01 00:07:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,7 +8,7 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  result = {}
+  result = []
   cart.each do |item|
     item.each do |name, info|
       if find_item_by_name_in_collection(item, result)
