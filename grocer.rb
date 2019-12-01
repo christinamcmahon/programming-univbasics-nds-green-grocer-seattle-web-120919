ee8bcf5bb2cbc1f50ab7e038be0bf96522e79afd
Christina McMahon <christina.mcmahon1@gmail.com>
2019-12-01 00:46:07 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,69 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection.each do |item|
+    if item[:item] == name
+      return item
+    end
+  end
+  nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  result = []
+  cart.each do |item|
+    found_item = find_item_by_name_in_collection(item[:item], result)
+    if found_item
+      found_item[:count] += 1
+    else
+      item[:count] = 1
+      result.push(item)
+    end
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    found_item = find_item_by_name_in_collection(coupon[:item], cart)
+    if found_item && found_item[:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        found_coupon_item = find_item_by_name_in_collection(new_name, cart)
+        if found_coupon_item
+            found_coupon_item[:count] += coupon[:num]
+        else
+          coupon_item = {
+            item: new_name,
+            price: coupon[:cost]/coupon[:num],
+            clearance: found_item[:clearance],
+            count: coupon[:num]
+          }
+          cart.push(coupon_item)
+        end
+        found_item[:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |item|
+    if item[:clearance]
+      item[:price] = (item[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  coupons_applied = apply_coupons(consolidated_cart, coupons)
+  clearance_applied = apply_clearance(coupons_applied)
+
+  total = 0.0
+  clearance_applied.each do |item|
+    total += item[:price] * item[:count]
+  end
+  if total > 100.00
+    total = total * 0.9
+  end
+  total.round(2)
 end
